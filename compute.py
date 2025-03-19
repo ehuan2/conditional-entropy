9c9503719da374f10776cc2dd9b768b3c1ebf43d
Eric Huang <eric.haoran.huang@gmail.com>
2025-03-18 20:46:57 -0400

    Add epsilon to compute to avoid divide by 0

--- compute.py
+++ compute.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 from grammar import PCFG
 from entropy import calc_inside, conditional_entropy
@@ -35,7 +36,13 @@ if __name__ == '__main__':
         prob_end = p[grammar.nt2idx[args.root]][0][i]
         ent_end = h[grammar.nt2idx[args.root]][0][i]
         prob = prob_notend + prob_end
-        ent = prob_end / prob * ent_end + prob_notend / prob * ent_notend + \
-            entropy(([prob_end / prob] if prob_end > 0 else []) +
-                [prob_notend / prob] if prob_notend > 0 else [])
+
+        ent = (
+            prob_end / (prob * ent_end + sys.float_info.epsilon) +
+            prob_notend / (prob * ent_notend + sys.float_info.epsilon) +
+            entropy(
+                ([prob_end / prob] if prob_end > 0 else []) +
+                [prob_notend / prob] if prob_notend > 0 else []
+            )
+        )
         print(output_format.format(prob, ent, args.sentence[i]))
